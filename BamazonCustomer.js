fec41274caec53e094483342bfa2320a6bf6a0cc
Matthew Mandel <matthewmandel@Matthew-Mandels-MacBook-Pro.local>
2016-10-25 22:24:40 -0400

    README1.md added

--- BamazonCustomer.js
+++ BamazonCustomer.js
@@ -1,5 +1,3 @@
-// having trouble grabbing answer from amount function and comparing it to 'order'; see line 41-43;
-
 var mysql = require('mysql');
 var prompt = require('prompt');
 var inquirer = require('inquirer');
@@ -32,21 +30,15 @@ connection.query('SELECT * FROM Products', function(err, res) {
       inquirer.prompt({
           name: "order",
           type: "input",
-          message: "Please enter the product ID number. "
+          message: "\nPlease enter the product ID number.\n "
       }).then(function(answer) {
-              // console.log(answer['order']);
-      
-
+             
       inquirer.prompt({
           name: "quantity",
           type: "input",
-          message: "Please enter the required quantity. "
+          message: "\nPlease enter the required quantity.\n\n "
       }).then(function(answer2) {
-              // console.log(answer2['quantity']);
-              // READ
-              // console.log(answer.order);
-              // console.log(answer2.quantity);
-
+             
       connection.query('SELECT StockQuantity FROM Products WHERE ?', [{ItemID: answer.order}], function(err, res){
         if(err) throw err;
      
@@ -62,23 +54,18 @@ connection.query('SELECT * FROM Products', function(err, res) {
           connection.query('SELECT Price FROM Products WHERE?', [{ItemID:answer.order}], function(err, res){
             if(err) throw err;
 
-            // console.log(res[0].Price);
             var price = res[0].Price;
-            console.log('Order Successful! Total purchase cost: $' + answer2.quantity*price);
-            // reRun();
+            console.log('\n\nOrder Successful! Total purchase cost: $' + answer2.quantity*price+'\n\n');
+            main();
+           
 
           });
 
-          // console.log('Order Successful! Total purchase cost: $' + answer2.quantity*price);
           var x = res[0].StockQuantity-answer2.quantity;
-          // console.log(x);
 
           connection.query('UPDATE Products SET StockQuantity = ? WHERE ItemID = ?', [x, answer.order], function(err, res){
-            // console.log(answer.order);
 
             if(err) throw err;
-
-            // console.log('updated quantity');
           }); 
 
       
@@ -89,27 +76,6 @@ connection.query('SELECT * FROM Products', function(err, res) {
   });
 
 };
-
-    
-
-    // function reRun(){
-
-    //       inquirer.prompt({
-    //       name: "more",
-    //       type: "input",
-    //       message: "If you wish to make another purchase please press 1. If not press 2."
-    //       }).then(function(answer3) {
-
-    //         if(answer3.name=1){
-    //           main();
-    //     }
-
-    //         else{
-    //           console.log('Have a nice day. Goodbye!');
-    //           // return false;
-    //         }
-    //   })
-    // };
 
 
 
---
+++ README1.md
@@ -0,0 +1,17 @@
+#Bamazon
+
+##Bamazon Customer View:
+
+###When the application begins the database table is displayed:
+
+![alt tag](images/customer)
+
+###The customer is prompted to enter the product ID number and the quantity they would like:
+
+![alt tag](images/customer1)
+
+###Since the product is available in sufficient quantity, the order total is displayed. The program begins again, allowing for further purchases. Note that the stockQuantity updated according to the previous purchase (Televisions decreased from 40 to 37).
+
+![alt tag](images/customer2)
+
+###When the user requests a higher quantity of a product than is available, "Insufficient Quantity!" is displayed. The table displays again and the user is prompted to make another purchase.
